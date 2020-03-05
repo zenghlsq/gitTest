a84a6ba9ddabfe9d93359e72a03d697fd95f559d
zenghlsq <894209123@qq.com>
2020-03-05 20:45:16 +0800

    启动类加初始化war

--- src/main/java/com/zeng/git_demo/GitDemoApplication.java
+++ src/main/java/com/zeng/git_demo/GitDemoApplication.java
@@ -2,13 +2,20 @@ package com.zeng.git_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class GitDemoApplication {
+public class GitDemoApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GitDemoApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitDemoApplication.class, args);
