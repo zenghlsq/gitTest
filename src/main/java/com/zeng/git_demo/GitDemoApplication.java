808fabbf6c3d93a3eb4adef2a309ef707eab04aa
zenghlsq <894209123@qq.com>
2020-03-05 21:02:12 +0800

    可以正常访问

--- src/main/java/com/zeng/git_demo/GitDemoApplication.java
+++ src/main/java/com/zeng/git_demo/GitDemoApplication.java
@@ -3,6 +3,7 @@ package com.zeng.git_demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@ServletComponentScan
 public class GitDemoApplication extends SpringBootServletInitializer {
 
 	@Override
